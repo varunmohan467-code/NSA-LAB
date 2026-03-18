325a72a50eb0ffe3d8546f773e1b105e8b687ba1
varunmohan467-code <varunmohan467@gmail.com>
2026-03-18 14:52:12 +0530

    Add files via upload

---
+++ program1.sh
@@ -0,0 +1,12 @@
+Check_odd_even(){
+ if [ $((number % 2)) -eq 0 ]; then
+   echo "$number is even"
+ else
+   echo "$number is odd"
+ fi
+}
+
+echo "Please enter a number:"
+read number
+
+Check_odd_even "$number"
